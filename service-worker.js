2fdbc00c3489a6fc598d99c6cbd6a8622da06f4e
umedturshiev-creator <umedturshiev@gmail.com>
2026-03-02 15:06:41 +0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,7 @@
-const CACHE_NAME = "fsl-crm-v1";
-
+const CACHE_NAME = "fsl-crm-v2";
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.add("./index.html"))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.add("./index.html")));
 });
-
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
