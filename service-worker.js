e047cf14b68cf72077ada23f8065c138cd03ec15
umedturshiev-creator <umedturshiev@gmail.com>
2026-03-02 15:10:31 +0500

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-const CACHE_NAME = "fsl-crm-v2";
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.add("./index.html")));
-});
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
-});
